39ee7db783cfab5010e4d43a7a11d48966898aa0
Isaac Hunter <ikehunter5@gmail.com>
2026-09-19 17:22:12 -0400

    create user crud apis

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,24 +41,26 @@ async def session_fixture(engine):  # noqa: ARG001
     """Create test db session with postgres test database."""
 
     async for db_session in get_db_session():
-        # Before each test, truncate db tables
-        conn = await db_session.connection()
-        inspector = await conn.run_sync(inspect)
-        target_tables = await conn.run_sync(
-            lambda _: inspector.get_sorted_table_and_fkc_names()  # noqa: B023
-        )
-        target_table_names = [
-            table[0] for table in target_tables if table[0] is not None
-        ]
+        try:
+            # Before each test, truncate db tables
+            conn = await db_session.connection()
+            inspector = await conn.run_sync(inspect)
+            target_tables = await conn.run_sync(
+                lambda _: inspector.get_sorted_table_and_fkc_names()  # noqa: B023
+            )
+            target_table_names = [
+                table[0] for table in target_tables if table[0] is not None
+            ]
 
-        for table in target_table_names:
-            await conn.execute(text(f'TRUNCATE TABLE "{table}" CASCADE'))
+            for table in target_table_names:
+                await conn.execute(text(f'TRUNCATE TABLE "{table}" CASCADE'))
 
-        # Pass db connection to the test
-        yield db_session
+            yield db_session
 
-        # After each test, roll back the changes
-        await db_session.rollback()
+            # After each test, roll back the changes
+            await db_session.rollback()
+        except Exception:
+            await db_session.rollback()
 
 
 @pytest.fixture(scope="function", name="client")
@@ -66,7 +68,8 @@ async def client_fixture(db: AsyncSession):
     """Create test api client with custom dependency overrides."""
 
     async def get_session_override():
-        return db
+        async with db.begin_nested() as transaction:
+            yield transaction.session
 
     # Make sure the api functions get the test database
     app.dependency_overrides[get_db_session] = get_session_override
