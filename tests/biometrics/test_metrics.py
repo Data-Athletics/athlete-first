a758e76d93dd948985f55af50e3be9534427ab2d
Ryan Froug <ryanpfroug@gmail.com>
2026-09-22 10:09:24 -0400

    feat: user relates to biometrics

--- tests/biometrics/test_metrics.py
+++ tests/biometrics/test_metrics.py
@@ -1,19 +1,22 @@
 import pytest
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.biometrics.dtos.HRVResponseDTO import HRVResponseDTO
 from app.core.dtos import SimpleResponseDTO
 from tests.mock.biometrics.metrics import EXPECTED_HRV
-
-MOCK_USER_ID = 10
-BASE_METRICS_ENDPOINT = f"/biometrics/{MOCK_USER_ID}"
+from tests.utils import create_test_user
 
 
-async def test_metrics_calculation(client: AsyncClient):
+async def test_metrics_calculation(client: AsyncClient, db: AsyncSession):
     """Check that metrics are calculated as expected"""
+
+    test_user = await create_test_user(db)
+    metrics_endpoint = f"/biometrics/{test_user.id}"
+
     with open("tests/fixtures/noop_mock.csv", "rb") as file:
         creation_response = await client.post(
-            f"/biometrics/{MOCK_USER_ID}",
+            metrics_endpoint,
             files={"file": ("noop_mock.csv", file, "text/csv")},
         )
 
@@ -21,7 +24,7 @@ async def test_metrics_calculation(client: AsyncClient):
         assert creation_response.status_code == 201
 
     hrv_response = await client.get(
-        BASE_METRICS_ENDPOINT + "/hrv" + "?end_time=" + "2025-04-27T14:46:47.336Z"
+        metrics_endpoint + "/hrv" + "?end_time=" + "2025-04-27T14:46:47.336Z"
     )
     print(hrv_response.json())
     hrv = HRVResponseDTO.model_validate(hrv_response.json())
